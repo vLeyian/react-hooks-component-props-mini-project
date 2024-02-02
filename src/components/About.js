7b11acb733f405a6b38f440e8f84ce842465d826
Leyian <victor.leyian@student.moringaschool.com>
2024-02-02 15:55:42 +0300

    completed task

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-function About({image = "https://via.placeholder.com/215Links to an external site."}){
-    return(
-        <aside>
-            <img src={image} alt="blog logo"></img>
-            <p>{About}</p>
-        </aside>
-    );
+function About({image="https://via.placeholder.com/215", about}){
+    return <aside>
+        <img src={image} alt="blog logo"/>
+        <p>{about}</p>
+    </aside>
 }
 
 export default About
